fcac32f74847910aef506722ddc9f4b9604456fe
jonnung <jonnung@gmail.com>
2020-11-08 23:15:01 +0900

    Add multiple routing rules

--- httpd.py
+++ httpd.py
@@ -4,11 +4,22 @@ import socketserver
 
 class RequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
+        try:
+            if self.path == '/':
+                html = "<html><head></head><body><h1>It works</h1></body></html>"
+            elif self.path == '/health':
+                html = "<html><head></head><body><h1>I'm alive~</h1></body></html>"
+            else:
+                raise Exception('Not Found')
+        except Exception:
+            html = "<html><head></head><body><h1>Not Found</h1></body></html>"
+            self.send_response(404)
+        else:
+            self.send_response(200)
+        finally:
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
 
-        html = "<html><head></head><body><h1>It works</h1></body></html>"
         self.wfile.write(bytes(html, "utf8"))
         return
 
